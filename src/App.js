0a53e31300ceea19aa96317363192f921ee6a39b
salman-threestops <salman.faris@threestops.com>
2022-05-21 11:52:12 +0530

    customer

--- src/App.js
+++ src/App.js
@@ -1,52 +1,35 @@
 import React from 'react';
  
 import {
-  BrowserRouter as Router,
+  BrowserRouter ,
   Switch,
   Route,
-  Redirect,
+  Routes,
+  Link,
+ 
 } from 'react-router-dom';
 
 import Home from './components/Home';
-import Form from 'react-bootstrap/Form';
-import { Button } from 'react-bootstrap';
+import CreateCustomer from './components/Customer/Create';
+import UpdateCustomer from './components/Customer/Update';
+
+
 import './App.css';
 
 function App() {
+
+  const onchange = () => {
+    
+  }
+
   return (
-    // <Router>
-    //   <Route exact path="/" element={<Home />}></Route>
-    // </Router>
-    <Form className="m-5">
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Customer name</Form.Label>
-        <Form.Control type="text" placeholder="Enter name" />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Customer Id</Form.Label>
-        <Form.Control type="text" />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Phone Number</Form.Label>
-        <Form.Control type="number" />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Address</Form.Label>
-        <Form.Control type="text" />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Place</Form.Label>
-        <Form.Control type="text" />
-      </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/customer/create" element={<CreateCustomer />} />
+        <Route exact path="/customer/update/" element={<UpdateCustomer />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
